6ffd5448dfcbd555b83d20280eeb74cb5733f78d
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-02 19:02:14 +0500

    NumberValidatorTests рефактор

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Collections;
+using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -7,25 +8,85 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
+    [Test, TestCaseSource(nameof(ValidatorTestCases))]
+    public bool IsValid(NumberValidator numberValidator, string number)
+    {
+        return numberValidator.IsValidNumber(number);
+    }
+    
+    public static IEnumerable ValidatorTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new NumberValidator(17, 2, true), "0.0")
+                .SetName("precision=17, scale=2, onlyPositive=true, number=0.0")
+                .Returns(true);
+            
+            yield return new TestCaseData(new NumberValidator(17, 2, true), "0")
+                .SetName("precision=17, scale=2, onlyPositive=true, number=0")
+                .Returns(true);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "00.00")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=00.00")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "-0.00")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=-0.00")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "+0.00")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=+0.00")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(4, 2, true), "+1.23")
+                .SetName("precision=4, scale=2, onlyPositive=true, number=+1.23")
+                .Returns(true);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "+1.23")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=+1.23")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(17, 2, true), "0.000")
+                .SetName("precision=17, scale=2, onlyPositive=true, number=0.000")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "-1.23")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=-1.23")
+                .Returns(false);;
+           
+            yield return new TestCaseData(new NumberValidator(3, 2, true), "a.sd")
+                .SetName("precision=3, scale=2, onlyPositive=true, number=a.sd")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(4, 2), "-1.23")
+                .SetName("precision=4, scale=2, onlyPositive=false, number=-1.23")
+                .Returns(true);
+            
+            yield return new TestCaseData(new NumberValidator(3, 2), "-1.23")
+                .SetName("precision=3, scale=2, onlyPositive=false, number=-1.23")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(4, 2), "a.sd")
+                .SetName("precision=4, scale=2, onlyPositive=false, number=a.sd")
+                .Returns(false);
+
+            yield return new TestCaseData(new NumberValidator(3, 2), "-0.00")
+                .SetName("precision=3, scale=2, onlyPositive=false, number=-0.00")
+                .Returns(false);
+            
+            yield return new TestCaseData(new NumberValidator(4, 2), "-0.00")
+                .SetName("precision=4, scale=2, onlyPositive=false, number=-0.00")
+                .Returns(true);
+        }
+    }
+
     [Test]
     public void Test()
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+        var wrongCreation = () => new NumberValidator(-1, 2, true);
+        var rightCreation = () => new NumberValidator(1, 0, true);
+        
+        wrongCreation.Should().Throw<ArgumentException>();
+        rightCreation.Should().NotThrow<ArgumentException>();
     }
 }
