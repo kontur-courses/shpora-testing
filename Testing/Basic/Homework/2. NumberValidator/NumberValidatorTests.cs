1ecadbcaf42846dfbcf009c01b92b50c976d4f63
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-03 13:19:44 +0500

    NumberValidatorTests рефактор теста для метода IsValidNumber: - Параметризация через TestCase - Убраны повторяющиеся тесты - Добавлены недостающие тесты

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using FluentAssertions;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.NumberValidator;
@@ -8,76 +10,45 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [TestCaseSource(nameof(ValidatorTestCases))]
-    public bool IsValid(NumberValidator numberValidator, string number)
-    {
-        return numberValidator.IsValidNumber(number);
-    }
-    
-    public static IEnumerable ValidatorTestCases
-    {
-        get
-        {
-            yield return new TestCaseData(new NumberValidator(17, 2, true), "0.0")
-                .SetName("precision=17, scale=2, onlyPositive=true, number=0.0")
-                .Returns(true);
-            
-            yield return new TestCaseData(new NumberValidator(17, 2, true), "0")
-                .SetName("precision=17, scale=2, onlyPositive=true, number=0")
-                .Returns(true);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "00.00")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=00.00")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "-0.00")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=-0.00")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "+0.00")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=+0.00")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(4, 2, true), "+1.23")
-                .SetName("precision=4, scale=2, onlyPositive=true, number=+1.23")
-                .Returns(true);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "+1.23")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=+1.23")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(17, 2, true), "0.000")
-                .SetName("precision=17, scale=2, onlyPositive=true, number=0.000")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "-1.23")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=-1.23")
-                .Returns(false);;
-           
-            yield return new TestCaseData(new NumberValidator(3, 2, true), "a.sd")
-                .SetName("precision=3, scale=2, onlyPositive=true, number=a.sd")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(4, 2), "-1.23")
-                .SetName("precision=4, scale=2, onlyPositive=false, number=-1.23")
-                .Returns(true);
-            
-            yield return new TestCaseData(new NumberValidator(3, 2), "-1.23")
-                .SetName("precision=3, scale=2, onlyPositive=false, number=-1.23")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(4, 2), "a.sd")
-                .SetName("precision=4, scale=2, onlyPositive=false, number=a.sd")
-                .Returns(false);
 
-            yield return new TestCaseData(new NumberValidator(3, 2), "-0.00")
-                .SetName("precision=3, scale=2, onlyPositive=false, number=-0.00")
-                .Returns(false);
-            
-            yield return new TestCaseData(new NumberValidator(4, 2), "-0.00")
-                .SetName("precision=4, scale=2, onlyPositive=false, number=-0.00")
-                .Returns(true);
-        }
+    [TestCase("0", 1, 0, true, ExpectedResult = true, 
+        TestName = "number=0; precision=1; scale=0; onlyPositive=true" )]
+    [TestCase("0.1", 2, 1, true, ExpectedResult = true,
+        TestName = "number=0.1; precision=2; scale=1; onlyPositive=true" )]
+    [TestCase("0,1", 2, 1, true, ExpectedResult = true,
+        TestName = "number=0,1; precision=2; scale=1; onlyPositive=true" )]
+    [TestCase("-1", 2, 0, false, ExpectedResult = true,
+        TestName = "number=-1; precision=2; scale=0; onlyPositive=false" )]
+    [TestCase("-1.1", 3, 1, false, ExpectedResult = true,
+        TestName = "number=-1.1; precision=3; scale=1; onlyPositive=false")]
+    [TestCase("-1,1", 3, 1, false, ExpectedResult = true,
+        TestName = "number=-1,1; precision=3; scale=1; onlyPositive=false")]
+    [TestCase("+1", 2, 0, true, ExpectedResult = true,
+        TestName = "number+1; precision=2; scale=0; onlyPositive=true")]
+    [TestCase("+1.1", 3, 1, true, ExpectedResult = true,
+        TestName = "number+1.1; precision=3; scale=1; onlyPositive=true")]
+    [TestCase("+1,1", 3, 1, true, ExpectedResult = true,
+        TestName = "number+1,1; precision=3; scale=1; onlyPositive=true")]
+    
+    [TestCase("", 2, 1, true, ExpectedResult = false,
+        TestName = "number=\"\"; precision=2; scale=1; onlyPositive=true")]
+    [TestCase(".0", 2, 1, true, ExpectedResult = false,
+        TestName = "number=.0; precision=2; scale=1; onlyPositive=true")]
+    [TestCase("0.", 2, 1, true, ExpectedResult = false,
+        TestName = "number=0.; precision=2; scale=1; onlyPositive=true")]
+    [TestCase("-1", 2, 0, true, ExpectedResult = false,
+        TestName = "number=-1; precision=2; scale=0; onlyPositive=true" )]
+    [TestCase("a.a", 2, 1, true, ExpectedResult = false,
+        TestName = "number=a.a; precision=2; scale=1; onlyPositive=true")]
+    [TestCase("1.1.1", 3, 2, true, ExpectedResult = false,
+        TestName = "number=1.1.1; precision=3; scale=2; onlyPositive=true")]
+    [TestCase("23,1", 2, 1, true, ExpectedResult = false,
+        TestName = "number=23,1; precision=2; scale=1; onlyPositive=true")]
+    [TestCase("23,1", 3, 0, true, ExpectedResult = false,
+        TestName = "number=23,1; precision=3; scale=0; onlyPositive=true")]
+    public bool IsValid(string number, int precision, int scale, bool onlyPositive)
+    {
+        return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number);
     }
 
     [TestCase(-1, 2, true, TestName = "negative precision")]
