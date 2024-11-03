1c75d103029fe8caaad02868aa5af26c5f3bc7a4
Савелий <vildanov.s@bk.ru>
2024-11-03 23:46:41 +0500

    did NumberValidator

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,31 +1,48 @@
-﻿
-using NUnit.Framework;
-using NUnit.Framework.Legacy;
+﻿using NUnit.Framework;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    [TestCase(-1,2,true,ExpectedResult = true,TestName = "Negative precision")]
+    [TestCase(1,0,true,ExpectedResult = false,TestName = "Not catch Exception")]
+    [TestCase(2,5,true,ExpectedResult = true,TestName = "Precision less then scale")]
+    [TestCase(2,-3,true,ExpectedResult = true,TestName = "Negative scale")]
+    public bool NumberValidator_Exceptions_Test(int precision, int scale, bool onlyPositive)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        try
+        {
+            new NumberValidator(precision, scale, onlyPositive);
+        }
+        catch (ArgumentException)
+        {
+            return true;
+        }
+        
+        return false;
+    }
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [Test, TestCaseSource(nameof(NumberValidatorTestCases))]
+    public bool IsValidNumber(NumberValidator numberValidator, String number)
+    {
+        return numberValidator.IsValidNumber(number);
+    }
+
+    public static IEnumerable<TestCaseData> NumberValidatorTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(new NumberValidator(8, 2, true), "0.0").Returns(true);
+            yield return new TestCaseData(new NumberValidator(3, 0, true), "0").Returns(true);
+            yield return new TestCaseData(new NumberValidator(4, 2, true), "-1.23").Returns(false);
+            yield return new TestCaseData(new NumberValidator(4, 2, false), "-1.24").Returns(true);
+            yield return new TestCaseData(new NumberValidator(5, 2, true), "1.253").Returns(false);
+            yield return new TestCaseData(new NumberValidator(3, 2, false), "a.ds").Returns(false); 
+            yield return new TestCaseData(new NumberValidator(4, 2, true), "").Returns(false);
+            yield return new TestCaseData(new NumberValidator(5, 2, true), "+-1.25").Returns(false);
+            yield return new TestCaseData(new NumberValidator(5, 2, true), null).Returns(false);
+            yield return new TestCaseData(new NumberValidator(3, 0, true), "1234").Returns(false);
+        }
     }
 }
