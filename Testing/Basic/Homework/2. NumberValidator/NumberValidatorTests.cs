3cdf7924f0ab7339595287124ce86d7e1375e54e
Geratoptus <shura.bocharov.2004@mail.ru>
2024-11-03 16:35:43 +0500

    NumberValidatorTests добавка краевых случаев

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -29,6 +29,7 @@ public class NumberValidatorTests
     [TestCase("1.1.1", 3, 2, true, ExpectedResult = false)]
     [TestCase("23,1", 2, 1, true, ExpectedResult = false)]
     [TestCase("23,1", 3, 0, true, ExpectedResult = false)]
+    [TestCase("+1", 1, 0, true, ExpectedResult = false)]
     public bool IsValid(string number, int precision, int scale, bool onlyPositive)
     {
         return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(number);
@@ -36,7 +37,9 @@ public class NumberValidatorTests
 
     [TestCase(-1, 2, true, TestName = "negative precision")]
     [TestCase(1, -2, true, TestName = "negative scale")]
-    [TestCase(1, 2, true, TestName = "scale >= precision")]
+    [TestCase(1, 2, true, TestName = "scale > precision")]
+    [TestCase(1, 1, true, TestName = "scale == precision")]
+    [TestCase(0, 0, false, TestName = "precision == 0")]
     public void NumberValidator_ThrowsException_AfterWrongCreation(int precision, int scale, bool onlyPositive)
     {
         var wrongCreation = () => new NumberValidator(precision, scale, onlyPositive);
