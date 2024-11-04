cfaf4bb46dbef810eba01f7a26662d093376ed84
Kirill Zaripov <kirill.zaripow@gmail.com>
2024-11-04 16:44:13 +0500

    fix

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
@@ -15,16 +14,15 @@ public class ObjectComparison
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
-        const string idName = nameof(Person.Id);
-        var regex = new Regex($@"^{idName}$|\.{idName}$");
-
         actualTsar.Should()
-            .BeEquivalentTo(expectedTsar, options => options.Excluding(su => regex.IsMatch(su.Path)));
+            .BeEquivalentTo(expectedTsar, options => options.Excluding(su => su.DeclaringType == typeof(Person) && su.Name == nameof(Person.Id)));
     }
 
     /*
      * 1) При добавлении новых полей в Person придется переписывать AreEquals
      * 2) Нету трейса какое поле не совпало, только общий результат
+     * 3) Из-за цикличной рекурсии тест зависнет, вариант выше упадет и напишет про цикл
+     * Без указания AllowingInfiniteRecursion FluentAssertions рекурсивно зайдет только в глубину на 10
      */
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -15,7 +15,7 @@ public class NumberValidatorTests
     {
         var act = () => new NumberValidator(precision, scale, onlyPositive);
 
-        act.Should().Throw<ArgumentException>();
+        act.Should().Throw<ArgumentException>("precision must be a positive number");
     }
 
     [TestCase(1, 2, true)]
@@ -24,39 +24,114 @@ public class NumberValidatorTests
     {
         var act = () => new NumberValidator(precision, scale, onlyPositive);
 
-        act.Should().Throw<ArgumentException>();
+        act.Should().Throw<ArgumentException>("precision must be a non-negative number less or equal than precision");
     }
-    
+
     [TestCase(5, -1, true)]
     [TestCase(5, -1, false)]
     public void ThrowArgumentException_WhenScaleIsLessThatZero(int precision, int scale, bool onlyPositive)
     {
         var act = () => new NumberValidator(precision, scale, onlyPositive);
 
-        act.Should().Throw<ArgumentException>();
+        act.Should().Throw<ArgumentException>("precision must be a non-negative number less or equal than precision");
     }
 
+    [TestCase("0.00")]
     [TestCase("0.0")]
     [TestCase("0")]
     [TestCase("1.0")]
+    [TestCase("1.00")]
     [TestCase("0.1")]
+    [TestCase("0.10")]
     [TestCase("0.01")]
-    [TestCase("+1.0")]
-    [TestCase("+1")]
-    [TestCase("+1.00")]
-    public void IsValidNumber_IsTrue_OnValidCases(string value)
+    public void IsValidNumber_IsTrue_WhenOnlyPositiveTrueAndSimpleInput(string value)
     {
-        var validator = ValidatorRegistry.GetDefaultNumberValidator();
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: true);
+
+        validator.IsValidNumber(value).Should().BeTrue();
+    }
+    
+    [TestCase("0.00")]
+    [TestCase("0.0")]
+    [TestCase("0")]
+    [TestCase("1.0")]
+    [TestCase("1.00")]
+    [TestCase("0.1")]
+    [TestCase("0.10")]
+    [TestCase("0.01")]
+    public void IsValidNumber_IsTrue_WhenOnlyPositiveFalseAndSimpleInput(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: false);
 
         validator.IsValidNumber(value).Should().BeTrue();
     }
 
-    [TestCase("a.a")]
-    [TestCase("+a.a")]
-    [TestCase("aa.a")]
-    [TestCase("-aa.a")]
+    [TestCase("-0.00")]
+    [TestCase("-0.0")]
+    [TestCase("-0")]
+    [TestCase("-1.0")]
+    [TestCase("-1.00")]
+    [TestCase("-0.1")]
+    [TestCase("-0.10")]
+    [TestCase("-0.01")]
+    public void IsValidNumber_IsFalse_WhenOnlyPositiveTrueAndNegativeInput(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: true);
+
+        validator.IsValidNumber(value).Should().BeFalse();
+    }
+
+    [TestCase("-0.00")]
+    [TestCase("-0.0")]
+    [TestCase("-0")]
+    [TestCase("-1.0")]
+    [TestCase("-1.00")]
+    [TestCase("-0.1")]
+    [TestCase("-0.10")]
+    [TestCase("-0.01")]
+    public void IsValidNumber_IsTrue_WhenOnlyPositiveFalseAndNegativeInput(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: false);
+
+        validator.IsValidNumber(value).Should().BeTrue();
+    }
+
+    [TestCase("+0.00")]
+    [TestCase("+0.0")]
+    [TestCase("+0")]
+    [TestCase("+1.0")]
+    [TestCase("+1.00")]
+    [TestCase("+0.1")]
+    [TestCase("+0.10")]
+    [TestCase("+0.01")]
+    public void IsValidNumber_IsTrue_WhenOnlyPositiveTrueAndPositiveInput(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: true);
+
+        validator.IsValidNumber(value).Should().BeTrue();
+    }
+    
+    [TestCase("+0.00")]
+    [TestCase("+0.0")]
+    [TestCase("+0")]
+    [TestCase("+1.0")]
+    [TestCase("+1.00")]
+    [TestCase("+0.1")]
+    [TestCase("+0.10")]
+    [TestCase("+0.01")]
+    public void IsValidNumber_IsTrue_WhenOnlyPositiveFalseAndPositiveInput(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: false);
+
+        validator.IsValidNumber(value).Should().BeTrue();
+    }
+
     [TestCase("a")]
-    [TestCase("+a")]
+    [TestCase("aa")]
+    [TestCase("a.a")]
+    [TestCase("a.aa")]
+    [TestCase("aa.aa")]
+    [TestCase("aa.a")]
     public void IsValidNumber_IsFalse_WhenInputIsNotValue(string value)
     {
         var validator = ValidatorRegistry.GetDefaultNumberValidator();
@@ -64,34 +139,34 @@ public class NumberValidatorTests
         validator.IsValidNumber(value).Should().BeFalse();
     }
 
-    [TestCase("-0.00")]
-    [TestCase("-0.0")]
-    [TestCase("-1.0")]
-    [TestCase("-0.1")]
-    public void IsValidNumber_IsFalse_WhenOnlyPositiveTrueAndNegativeInput(string value)
+    [TestCase("+a")]
+    [TestCase("+aa")]
+    [TestCase("+a.a")]
+    [TestCase("+a.aa")]
+    [TestCase("+aa.aa")]
+    [TestCase("+aa.a")]
+    public void IsValidNumber_IsFalse_WhenInputIsNotValueWithPlus(string value)
     {
-        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: true);
+        var validator = ValidatorRegistry.GetDefaultNumberValidator();
 
         validator.IsValidNumber(value).Should().BeFalse();
     }
-    
-    [TestCase("-0.00")]
-    [TestCase("-0.0")]
-    [TestCase("-1.0")]
-    [TestCase("-0.1")]
-    public void IsValidNumber_IsTrue_WhenOnlyPositiveFalseAndNegativeInput(string value)
-    {
-        var validator = ValidatorRegistry.GetDefaultNumberValidator(onlyPositive: false);
 
-        validator.IsValidNumber(value).Should().BeTrue();
+    [TestCase("-a")]
+    [TestCase("-aa")]
+    [TestCase("-a.a")]
+    [TestCase("-a.aa")]
+    [TestCase("-aa.aa")]
+    [TestCase("-aa.a")]
+    public void IsValidNumber_IsFalse_WhenInputIsNotValueWithMinus(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator();
+
+        validator.IsValidNumber(value).Should().BeFalse();
     }
-    
+
     [TestCase("0000.01")]
-    [TestCase("-000.01")]
-    [TestCase("+000.01")]
     [TestCase("100000")]
-    [TestCase("-10000")]
-    [TestCase("+10000")]
     public void IsValidNumber_IsFalse_WhenValueExceedsPrecision(string value)
     {
         var validator = ValidatorRegistry.GetDefaultNumberValidator();
@@ -99,9 +174,25 @@ public class NumberValidatorTests
         validator.IsValidNumber(value).Should().BeFalse();
     }
     
+    [TestCase("+000.01")]
+    [TestCase("+10000")]
+    public void IsValidNumber_IsFalse_WhenPositiveValueExceedsPrecision(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator();
+
+        validator.IsValidNumber(value).Should().BeFalse();
+    }
+    
+    [TestCase("-000.01")]
+    [TestCase("-10000")]
+    public void IsValidNumber_IsFalse_WhenNegativeValueExceedsPrecision(string value)
+    {
+        var validator = ValidatorRegistry.GetDefaultNumberValidator();
+
+        validator.IsValidNumber(value).Should().BeFalse();
+    }
+
     [TestCase("0.0001")]
-    [TestCase("-0.0001")]
-    [TestCase("+0.0001")]
     [TestCase("0.0000")]
     public void IsValidNumber_IsFalse_WhenFractionExceedsScale(string value)
     {
@@ -110,6 +201,24 @@ public class NumberValidatorTests
         validator.IsValidNumber(value).Should().BeFalse();
     }
     
+    [TestCase("+0.0001")]
+    [TestCase("+0.0000")]
+    public void IsValidNumber_IsFalse_WhenPositiveFractionExceedsScale(string value)
+    {
+        var validator = new NumberValidator(99, 3);
+
+        validator.IsValidNumber(value).Should().BeFalse();
+    }
+    
+    [TestCase("-0.0001")]
+    [TestCase("-0.0000")]
+    public void IsValidNumber_IsFalse_WhenNegativeFractionExceedsScale(string value)
+    {
+        var validator = new NumberValidator(99, 3);
+
+        validator.IsValidNumber(value).Should().BeFalse();
+    }
+
     [Test]
     public void IsValidNumber_IsFalse_WhenInputIsEmpty()
     {
