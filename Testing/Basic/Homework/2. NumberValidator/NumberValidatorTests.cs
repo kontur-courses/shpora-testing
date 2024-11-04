475de0f14211bdea3e8977baf2d0d6e5fe64c2f4
Leonid <rtdddawda2534@yandex.ru>
2024-11-04 18:11:34 +0500

    Fix NumberValidatorTests

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,31 +1,52 @@
-﻿
+﻿using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
+    [TestCase(-1, 2, true, TestName = "negative precision")]
+    [TestCase(0, 2, false, TestName = "precision == 0")]
+    [TestCase(1, -2, true, TestName = "negative scale")]
+    [TestCase(1, 2, false, TestName = "scale > precision")]
+    [TestCase(1, 1, true, TestName = "scale == precision")]
+    public void NumberValidation_ConstructorHasArgumentException(int precision, int scale, bool onlyPositive)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        var action = () => new NumberValidator(precision, scale, onlyPositive);
+        action.Should().Throw<ArgumentException>();
+    }
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [TestCase(1, 0, true, TestName = "default")]
+    public void NumberValidation_ConstructorDoesNotHaveArgumentException(int precision, int scale, bool onlyPositive)
+    {
+        var action = () => new NumberValidator(precision, scale, onlyPositive);
+        action.Should().NotThrow();
+    }
+
+    [TestCase(1, 0, true, "0",ExpectedResult = true)]
+    [TestCase(2, 1, true, "0.0", ExpectedResult = true)]
+    [TestCase(2, 1, true, "0.1", ExpectedResult = true)]
+    [TestCase(2, 1, true, "0,1", ExpectedResult = true)]
+    [TestCase(2, 0, true, "+1", ExpectedResult = true)]
+    [TestCase(3, 1, true, "+1.1", ExpectedResult = true)]
+    [TestCase(3, 1, true, "+1,1", ExpectedResult = true)]
+    [TestCase(2, 0, false, "-1", ExpectedResult = true)]
+    [TestCase(3, 1, false, "-1.1", ExpectedResult = true)]
+    [TestCase(3, 1, false, "-1,1", ExpectedResult = true)]
+    [TestCase(7, 2, true, "671,23", ExpectedResult = true)]
+
+    [TestCase(2, 0, true, "", ExpectedResult = false)]
+    [TestCase(2, 1, true, ".0", ExpectedResult = false)]
+    [TestCase(2, 0, true, "0.", ExpectedResult = false)]
+    [TestCase(2, 0, true, "-1", ExpectedResult = false)]
+    [TestCase(10, 2, true, "abcde.f", ExpectedResult = false)]
+    [TestCase(10, 9, true, "127.0.0.1", ExpectedResult = false)]
+    [TestCase(3, 2, true, "4321.5", ExpectedResult = false)]
+    [TestCase(3, 0, true, "+145", ExpectedResult = false)]
+    [TestCase(3, 0, false, "-124", ExpectedResult = false)]
+    public bool IsValidNumber(int precision, int scale, bool onlyPositive, string value)
+    {
+        return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
     }
 }
