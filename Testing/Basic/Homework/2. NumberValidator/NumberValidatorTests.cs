e0e6f80b82f6c7ec816286a7333549714d501ba4
Maxim <mbrozovskii@gmail.com>
2024-11-05 01:23:42 +0500

    Refactor: NumberValidatorTests -- added some edge cases for constructor -- rework test schema for IsValidNumber -- added more testcases

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -7,110 +7,98 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void NumberValidator_ThrowsArgumentException_WhenPrecisionIsNegative()
+    private const string PrecisionErrorMessage = "precision must be a positive number";
+    private const string ScaleErrorMessage = "precision must be a non-negative number less or equal than precision";
+
+    [TestCase(-1, 0, PrecisionErrorMessage, Description = "precision < 0")]
+    [TestCase(-10, 1, PrecisionErrorMessage, Description = "precision < 0")]
+    [TestCase(0, 0, PrecisionErrorMessage, Description = "precision == 0")]
+    [TestCase(0, 2, PrecisionErrorMessage, Description = "precision == 0")]
+    [TestCase(5, -1, ScaleErrorMessage, Description = "scale < 0")]
+    [TestCase(5, -6, ScaleErrorMessage, Description = "scale < 0")]
+    [TestCase(1, 2, ScaleErrorMessage, Description = "scale > precision")]
+    [TestCase(1, 10, ScaleErrorMessage, Description = "scale > precision")]
+    [TestCase(1, 1, ScaleErrorMessage, Description = "scale == precision")]
+    [TestCase(2, 2, ScaleErrorMessage, Description = "scale == precision")]
+    public void NumberValidator_ThrowsArgumentException_WithInvalidParams(int precision, int scale, string msg)
     {
-        const string msg = "precision must be a positive number";
-        Action act = () => new NumberValidator(-1, 2); 
+        Action act = () => new NumberValidator(precision, scale);
         act.Should().Throw<ArgumentException>().WithMessage(msg);
     }
 
-    [Test]
-    public void NumberValidator_ThrowsArgumentException_WhenScaleLessThanZero()
+    [TestCase(17, 10)]
+    [TestCase(3, 2)]
+    [TestCase(1, 0)]
+    [TestCase(6, 5)]
+    [TestCase(300, 52)]
+    public void NumberValidator_DoesNotThrow_WithValidParams(int precision, int scale)
     {
-        const string msg = "precision must be a non-negative number less or equal than precision";
-        Action act = () => new NumberValidator(1, -2);
-        act.Should().Throw<ArgumentException>().WithMessage(msg);
-    }
-
-    [Test]
-    public void NumberValidator_ThrowsArgumentException_WhenScaleGreaterOrEqualThanPrecision()
-    {
-        const string msg = "precision must be a non-negative number less or equal than precision";
-        Action actWhenGreater = () => new NumberValidator(17, 18);
-        Action actWhenEqual = () => new NumberValidator(17, 17); 
-        actWhenGreater.Should().Throw<ArgumentException>().WithMessage(msg);
-        actWhenEqual.Should().Throw<ArgumentException>().WithMessage(msg);
-    }
-
-    [Test]
-    public void NumberValidator_DoesNotThrow_WhenPrecisionIsPositive()
-    {
-        Action act = () => new NumberValidator(1);
+        Action act = () => new NumberValidator(precision, scale);
         act.Should().NotThrow();
     }
 
-    [Test]
-    public void NumberValidator_DoesNotThrow_WhenScaleLessThanPrecision()
-    {
-        Action act = () => new NumberValidator(2, 1);
-        act.Should().NotThrow();
-    }
-
-    [TestCase("2", 1, true)]
-    [TestCase("52", 2, true)]
-    [TestCase("-300", 4, false)]
-    [TestCase("300", 3, false)]
-    [TestCase("-99", 3, false)]
-    [TestCase("9999999", 7, true)]
-    [TestCase("-999999999", 10, false)] 
-    public void IsValidNumber_ShouldBeTrue_WhenValueIsInteger(string value, int precision, bool onlyPositive) => 
-        TestShouldBeTrue(value, precision, 0, onlyPositive);
-    
-    [TestCase("52", 1, true)]
-    [TestCase("-300", 3, false)]
-    [TestCase("300", 2, false)]
-    [TestCase("-1234567890", 10, false)]
-    public void IsValidNumber_ShouldBeFalse_WhenValueIsInteger(string value, int precision, bool onlyPositive) =>
-        TestShouldBeFalse(value, precision, 0, onlyPositive);
-   
-    [TestCase("a.sd")]
-    [TestCase("seven.eleven")]
-    [TestCase("     ")]
-    [TestCase("five/two")]
-    [TestCase("")]
-    [TestCase(null)]
-    public void IsValidNumber_ShouldBeFalse_WhenValueIsNonNumber(string value) => TestShouldBeFalse(value);
-    
-    [TestCase("0.000", 4,3, true)]
+    [TestCase("2", 1, 0, true)]
+    [TestCase("52", 2, 1, true)]
+    [TestCase("-300", 4, 0, false)]
+    [TestCase("300", 3, 0, true)]
+    [TestCase("-99", 3, 2, false)]
+    [TestCase("9999999", 7, 1, false)]
+    [TestCase("2147483649", 10, 1, false, Description = "Int overflow")]
+    [TestCase("-999999999", 10, 1, false)]
+    [TestCase("-1234567890", 11, 8, false)]
+    [TestCase("0101010", 20, 12, false)]
+    [TestCase("0.000", 4, 3, true)]
     [TestCase("-1234567890.1234567890", 21, 10, false)]
-    [TestCase("0.0", 2, 1,false)]
+    [TestCase("0.0", 2, 1, false)]
     [TestCase("300.52", 5, 2, true)]
-    public void IsValidNumber_ShouldBeTrue_WhenValueIsFloatNumber(string value, int precision, int scale, 
-        bool onlyPositive) => TestShouldBeTrue(value, precision, scale, onlyPositive);
-    
-    [TestCase("-1.23", 3, 2, false)]
-    [TestCase("-1234567890.1234567890", 20, 10, false)]
-    [TestCase("100", 2, 1, true)]
-    public void IsValidNumber_ShouldBeFalse_WhenValueIsFloatNumber(string value, int precision, int scale, 
-        bool onlyPositive) => TestShouldBeFalse(value, precision, scale, onlyPositive);
-    
-    [TestCase("1.1")]
-    [TestCase("300,52")]
-    public void IsValidNumber_ShouldBeTrue_WhenFormatOfValueIsOk(string value) => TestShouldBeTrue(value);
-    
-    [TestCase("X")]
-    [TestCase("X,Y")]
-    [TestCase("1\n.5")]
-    [TestCase("+-15")]
-    [TestCase("300_000")]
-    [TestCase("52 000")]
-    [TestCase("1/2")]
-    [TestCase("12 . 22")]
-    public void IsValidNumber_ShouldBeFalse_WhenFormatOfValueIsNotOk(string value) => TestShouldBeFalse(value);
-    
-    [TestCase("+1.23")]
-    [TestCase("+300.52")]
-    [TestCase("-999.99")]
-    [TestCase("-101")]
-    public void IsValidNumber_ShouldBeTrue_WhenValueHasSign(string value) => TestShouldBeTrue(value);
-    
-    private static void TestShouldBeTrue(string value, int precision = 17, int scale = 10, bool onlyPositive = false) => 
+    [TestCase("0.0", 2, 1, false)]
+    [TestCase("0.000", 4, 3, true)]
+    [TestCase("300.52", 5, 2, true)]
+    [TestCase("+1.23", 17, 10, false)]
+    [TestCase("+300.52", 17, 10, false)]
+    [TestCase("300,52", 17, 10, false)]
+    [TestCase("1,23", 17, 10, false)]
+    [TestCase("100", 3, 1, true)]
+    public void IsValidNumber_ShouldBeTrue_WithValidParams(string value, int precision, int scale, bool onlyPositive) =>
         new NumberValidator(precision, scale, onlyPositive)
             .IsValidNumber(value)
             .Should().BeTrue();
 
-    private static void TestShouldBeFalse(string value, int precision = 17, int scale = 10, bool onlyPositive = false) =>
+    [TestCase("", 17, 10, false)]
+    [TestCase(null, 17, 10, false)]
+    [TestCase("II", 17, 10, false)]
+    [TestCase("III", 17, 10, false)]
+    [TestCase("VI", 17, 10, false)]
+    [TestCase("X", 17, 10, false)]
+    [TestCase("seven.eleven", 17, 10, false)]
+    [TestCase("five/two", 17, 10, false)]
+    [TestCase("11.11.11", 17, 10, false)]
+    [TestCase("11.22,33", 17, 10, false)]
+    [TestCase("55,22,33", 17, 10, false)]
+    [TestCase("1/2", 17, 10, false)]
+    [TestCase("-100", 4, 0, true)]
+    [TestCase("-0.00", 3, 2, true)]
+    [TestCase("+1,23", 3, 2, true)]
+    [TestCase("1.123", 4, 2, true)]
+    [TestCase("0.000", 5, 2, true)]
+    [TestCase("a.sd", 17, 10, false)]
+    [TestCase("52", 1, 0, true)]
+    [TestCase("-300", 3, 0, false)]
+    [TestCase("300", 2, 0, false)]
+    [TestCase("-1234567890", 10, 0, false)]
+    [TestCase("-1.23", 3, 2, false)]
+    [TestCase("-1234567890.1234567890", 20, 10, false)]
+    [TestCase("100", 2, 1, true)]
+    [TestCase("X,Y", 3, 2, true)]
+    [TestCase("1\n.5", 3, 2, true)]
+    [TestCase("+-15", 3, 2, false)]
+    [TestCase("300_000", 17, 10, false)]
+    [TestCase("52 000", 17, 10, false)]
+    [TestCase("1/2", 17, 10, false)]
+    [TestCase("12 . 22", 17, 10, false)]
+    [TestCase("12 , 22", 17, 10, false)]
+    public void IsValidNumber_ShouldBeFalse_WithInvalidParams(string value, int precision, int scale, 
+        bool onlyPositive) =>
         new NumberValidator(precision, scale, onlyPositive)
             .IsValidNumber(value)
             .Should().BeFalse();
