d00183c4791c59211f0780d6cd3d58c9764a152a
Leonid <rtdddawda2534@yandex.ru>
2024-11-04 15:31:05 +0500

    Fix ObjectComparison

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
@@ -14,16 +15,8 @@ public class ObjectComparison
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
-        // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+            options.Excluding(o => o.Name == "Id" && o.DeclaringType == typeof(Person)));
     }
 
     [Test]
@@ -35,6 +28,18 @@ public class ObjectComparison
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
         // Какие недостатки у такого подхода? 
+        /*
+         1) Метод AreEqual проверяет только фиксированые параметры в Person, поэтому
+            если класс Person будет измненён, то тесты предётся переписывать,
+            добавляя новые или удаляя старые поля.
+         2) Метод AreEqual возвращает True или False, поэтому даже если только одно поле
+            не пройдёт проверку, всё что будет понятно, что проверка не прошла, а
+            что именно было не так, узнать не получится.
+         3) Даже если переписывание тестов не является проблемой (см. 1)), то при добавлении
+            большего количества полей, особенно если типом новых полей будут другие классы
+            со своими полями, метод AreEqual рискует разрастись до монструозных масштабов,
+            из-за чего его станет очень сложно читать.
+        */
         ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
     }
 
