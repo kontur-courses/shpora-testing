2c16af42a9c4965d51e60ed4eab3aff83c2425ad
devilboi95 <turnickii.n@gmail>
2024-11-04 11:55:39 +0500

    почти HotFix ObjectComparison.cs

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,44 +1,50 @@
-﻿using HomeExercises;
+﻿using System.Reflection;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
+
 public class ObjectComparison
-	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = TsarRegistry.GetCurrentTsar();
-			
-			actualTsar.ShouldBe(expectedTsar);
-		}
-		
-		[Test]
-		[Description("Альтернативное решение. Какие у него недостатки?")]
-		public void CheckCurrentTsar_WithCustomEquality()
-		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-			var expectedTsar = TsarRegistry.GetCurrentTsar();
+{
+    [Test]
+    [Description("Проверка текущего царя")]
+    [Category("ToRefactor")]
+    public void CheckCurrentTsar()
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+        var expectedTsar = new Person(
+            "Ivan IV The Terrible", 54, 170, 70,
+            new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
-			// 
-			// если появится новое свойство, то нужно писать дополнительный код
-			// слишком много кода нужно писать
-			ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
-		}
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+            options.Excluding((IMemberInfo memberInfo) => memberInfo.Name == "Id"));
+    }
 
-		private bool AreEqual(Person? actual, Person? expected)
-		{
-			if (actual == expected) return true;
-			if (actual == null || expected == null) return false;
-			return // если появится новое свойство его нужно будет прописывать здесь
-				actual.Name == expected.Name
-				&& actual.Age == expected.Age
-				&& actual.Height == expected.Height
-				&& actual.Weight == expected.Weight
-				&& AreEqual(actual.Parent, expected.Parent); 
-		}
-	}
+    [Test]
+    [Description("Альтернативное решение. Какие у него недостатки?")]
+    public void CheckCurrentTsar_WithCustomEquality()
+    {
+        var actualTsar = TsarRegistry.GetCurrentTsar();
+        var expectedTsar = TsarRegistry.GetCurrentTsar();
+
+        // Какие недостатки у такого подхода? 
+        // 
+        // если появится новое свойство, то нужно писать дополнительный код
+        // слишком много кода нужно писать
+        ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
+    }
+
+    private bool AreEqual(Person? actual, Person? expected)
+    {
+        if (actual == expected) return true;
+        if (actual == null || expected == null) return false;
+        return // если появится новое свойство его нужно будет прописывать здесь
+            actual.Name == expected.Name
+            && actual.Age == expected.Age
+            && actual.Height == expected.Height
+            && actual.Weight == expected.Weight
+            && AreEqual(actual.Parent, expected.Parent);
+    }
+}
