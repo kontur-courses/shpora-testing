675078d26eff2ee7d42bc289bfc295c415df2bd3
Dmitriy Bessarab <bessarabds@gmail.com>
2024-11-05 18:43:16 +0500

    NumberValidatorTests.cs refactoring after review

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -39,73 +39,40 @@ public class NumberValidatorTests
             .WithMessage("precision must be a positive number");
     }
 
-    [Test]
-    public void NumberValidator_ExcludesNegativeScale()
+    [TestCase(2, -1)]
+    [TestCase(1, 2)]
+    public void NumberValidator_ExcludesNegativeOrWrongValueScale(int precision, int scale)
     {
-        Action numberValidator = () => new NumberValidator(2, -1);
+        Action numberValidator = () => new NumberValidator(precision, scale);
 
         numberValidator.Should().Throw<ArgumentException>()
             .WithMessage("precision must be a non-negative number less or equal than precision");
         //некорректное сообщение об ошибке в конструкторе NumberValidator, должно быть
             //"scale must be a non-negative number less than precision"
     }
-
-    [Test]
-    public void NumberValidator_ExcludesScaleGreaterOrEqualThanPrecision()
-    {
-        Action numberValidator = () => new NumberValidator(1, 2);
-
-        numberValidator.Should().Throw<ArgumentException>()
-            .WithMessage("precision must be a non-negative number less or equal than precision");
-        //некорректное сообщение аналагично предыдущему тесту
-    }
-
-    [Test]
-    public void Check_IsValidNumber_ReturnsFalseIfStringIsEmpty()
+    
+    [TestCase(17, 2, true, "")]
+    [TestCase(17, 2, true, null)]
+    public void Check_IsValidNumber_ReturnsFalseIfStringIsNullOrEmpty(int precision, int scale, bool onlyPositive, string number)
     {
         var result = new NumberValidator(17, 2, true)
-            .IsValidNumber(string.Empty);
+            .IsValidNumber(number);
 
         result.Should().BeFalse("Пустая строка в качестве аргумента");
     }
-
-    [Test]
-    public void Check_IsValidNumber_ReturnsFalseIfStringIsNull()
+    
+    [TestCase(3, 2, true, "-0.00")]
+    [TestCase(17, 2, true, "0.000")]
+    [TestCase(17, 2, true, "a.sd")]
+    public void Check_IsValidNumber_TheCorrectNumberFormat(int precision, int scale, bool onlyPositive, string number)
     {
-        var result = new NumberValidator(17, 2, true)
-            .IsValidNumber(null);
+        var result = new NumberValidator(precision, scale, onlyPositive)
+            .IsValidNumber(number);
 
-        result.Should().BeFalse("null в качестве аргумента");
+        result.Should().BeFalse("Неправильный формат числа");
     }
 
     [Test]
-    public void Check_IsValidNumber_ReturnsFalseIfNotANumber()
-    {
-        var result = new NumberValidator(17, 2, true)
-            .IsValidNumber("a.sd");
-
-        result.Should().BeFalse("Не число в качестве аргумента");
-    }
-
-    public void Check_IsValidNumber_CorrectNumberOfDigits()
-    {
-        var result = new NumberValidator(3, 2, true)
-            .IsValidNumber("-0.00");
-
-        result.Should().BeFalse("Неправильный формат числа, неправильное количество символов");
-    }
-
-    [Test]
-    public void Check_IsValidNumber_CorrectNumberOfFractionalDigits()
-    {
-        var result = new NumberValidator(17, 2, true)
-            .IsValidNumber("0.000");
-
-        result.Should().BeFalse("Неправильный формат чилса, неправильное количество знаков дробной части");
-    }
-
-    [Test]
-
     public void Check_IsValidNumber_TakesIntoAccountTheSign()
     {
         var result = new NumberValidator(4, 2, true)
