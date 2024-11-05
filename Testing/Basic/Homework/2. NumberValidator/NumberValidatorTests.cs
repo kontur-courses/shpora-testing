ec56affee52b138aa4f294a4177c7fbf12a68301
S4MPAI <flekshum@gmail.com>
2024-11-05 22:23:37 +0500

    merge IsValidNumber Tests in TestCases

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -27,48 +27,22 @@ public class NumberValidatorTests
         Assert.DoesNotThrow(() => builder.Build());
     }
 
-    [Test]
-    public void IsValidNumber_Should_Return_False_If_Value_Is_Null() =>
-        IsValidNumberShouldReturnFalse(null!, 10, 5);
-    
-    [TestCase("")]
+    [TestCase(null)]
     [TestCase(" ")]
-    [TestCase("    ")]
-    public void IsValidNumber_Should_Return_False_If_Value_Is_Empty(string value) => 
-        IsValidNumberShouldReturnFalse(value, 10, 5);
-    
     [TestCase("a.sd")]
     [TestCase("0.")]
     [TestCase(",0")]
     [TestCase("0*0")]
     [TestCase("!0,0")]
-    public void IsValidNumber_Should_Return_False_If_Match_With_Value_Fail(string value) => 
-        IsValidNumberShouldReturnFalse(value, 10, 5);
-    
     [TestCase("1,234")]
-    [TestCase("1111")]
+    [TestCase("11111")]
     [TestCase("22,32")]
-    [TestCase("+1,23")]
-    [TestCase("+0,00")]
-    [TestCase("-0,00")]
-    public void IsValidNumber_Should_Return_False_If_Sum_Of_IntPart_And_FracPart_More_Than_Precision(string value) => 
-        IsValidNumberShouldReturnFalse(value, 3, 2);
-
-    [Test]
-    public void IsValidNumber_Should_Return_False_If_FracPart_More_Than_Scale() =>
-        IsValidNumberShouldReturnFalse("0,000", 10, 2);
-    
-    [TestCase("-0,0")]
-    [TestCase("-0,000")]
+    [TestCase("+14,23")]
     [TestCase("-0")]
-    public void IsValidNumber_Should_Return_False_If_OnlyPositive_True_And_Value_Contains_Minus(string value) => 
-        IsValidNumberShouldReturnFalse(value, 10, 5);
-
-    private void IsValidNumberShouldReturnFalse(string value, int precision, int scale, bool onlyPositive = true)
+    public void IsValidNumber_Should_Return_False(string value)
     {
         var numberValidator = new TestNumberValidatorBuilder()
-            .WithPrecisionAndScale(precision, scale)
-            .WithOnlyPositive(onlyPositive)
+            .WithPrecisionAndScale(4, 2)
             .Build();
         
         var actual = numberValidator.IsValidNumber(value);
@@ -76,26 +50,13 @@ public class NumberValidatorTests
         actual.Should().BeFalse();
     }
     
-    [TestCase("-0,0")]
-    [TestCase("-0,000")]
-    [TestCase("-0")]
-    public void IsValidNumber_Should_Return_True_If_OnlyPositive_False_And_Value_Contains_Minus(string value)
+    [TestCase("-1,4", false)]
+    [TestCase("1")]
+    [TestCase("+1,444")]
+    [TestCase("1,444")]
+    public void IsValidNumber_Should_Return_True(string value, bool onlyPositive = true)
     {
-        var numberValidator = new TestNumberValidatorBuilder().WithOnlyPositive(false).Build();
-        
-        var actual = numberValidator.IsValidNumber(value);
-        
-        actual.Should().BeTrue();
-    }
-    
-    [TestCase("0.0")]
-    [TestCase("0")]
-    [TestCase("+1.23")]
-    [TestCase("1.444")]
-    [TestCase("+1.444")]
-    public void IsValidNumber_Should_Return_True_If_Value_Correct(string value)
-    {
-        var numberValidator = new TestNumberValidatorBuilder().Build();
+        var numberValidator = new TestNumberValidatorBuilder().WithOnlyPositive(onlyPositive).Build();
         
         var actual = numberValidator.IsValidNumber(value);
         
