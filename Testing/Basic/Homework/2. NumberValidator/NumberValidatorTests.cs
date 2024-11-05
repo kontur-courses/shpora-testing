6520b2ebe71b428fcd441cc8ac5aa703d75e2ba1
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-11-05 21:46:28 +0500

    Refactored NumberValidatorTests so it uses TestCase or TestCaseSource attributes

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using NUnit.Framework;
@@ -9,147 +10,193 @@ namespace HomeExercise.Tasks.NumberValidator;
 public class NumberValidatorTests
 {
     [Test]
-    public void NumberValidator_DoesNotThrowOnCreation_WithCorrectArguments()
+    [TestCase(1, 0, true)]
+    [TestCase(1, 0, false)]
+    [TestCase(12, 5, true)]
+    [TestCase(12, 5, false)]
+    public void NumberValidator_DoesNotThrowOnCreation_WithCorrectArguments(
+        int precision,
+        int scale,
+        bool onlyPositive)
     {
-        using (new AssertionScope())
-        {
-            new Action(() => new NumberValidator(1, 0, true)).Should().NotThrow();
-            new Action(() => new NumberValidator(1, 0, false)).Should().NotThrow();
-            new Action(() => new NumberValidator(12, 5, true)).Should().NotThrow();
-            new Action(() => new NumberValidator(12, 5, false)).Should().NotThrow();
-        }
+        new Action(() => new NumberValidator(precision, scale, onlyPositive))
+            .Should()
+            .NotThrow();
     }
 
     [Test]
-    public void NumberValidator_ThrowsOnCreation_WithScale_BeingMoreOrEqualTo_Precision()
+    [TestCase(1, 2, false)]
+    [TestCase(1, 2, true)]
+    [TestCase(7, 9, true)]
+    [TestCase(7, 9, false)]
+    [TestCase(7, 7, true)]
+    [TestCase(7, 7, false)]
+    public void NumberValidator_ThrowsOnCreation_WithScale_BeingMoreOrEqualTo_Precision(
+        int precision,
+        int scale,
+        bool onlyPositive)
     {
-        using (new AssertionScope())
-        {
-            new Action(() => new NumberValidator(1, 2, false)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(1, 2, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, 9, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, 9, false)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, 7, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, 7, false)).Should().Throw<ArgumentException>();
-        }
+        new Action(() => new NumberValidator(precision, scale, onlyPositive))
+            .Should()
+            .Throw<ArgumentException>()
+            .WithMessage("precision must be a non-negative number less or equal than precision");
     }
 
     [Test]
-    public void NumberValidator_ThrowsOnCreation_WithPrecision_BeingLessOrEqualTo_Zero()
+    [TestCase(-1, 2, false)]
+    [TestCase(-1, 2, true)]
+    [TestCase(0, 0, false)]
+    [TestCase(0, 0, true)]
+    public void NumberValidator_ThrowsOnCreation_WithPrecision_BeingLessOrEqualTo_Zero(
+        int precision,
+        int scale,
+        bool onlyPositive)
     {
-        using (new AssertionScope())
-        {
-            new Action(() => new NumberValidator(-1, 2, false)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(-1, 2, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(0, 0, false)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(0, 0, true)).Should().Throw<ArgumentException>();
-        }
+        new Action(() => new NumberValidator(precision, scale, onlyPositive))
+            .Should()
+            .Throw<ArgumentException>()
+            .WithMessage("precision must be a positive number");
     }
 
     [Test]
-    public void NumberValidator_ThrowsOnCreation_WithScale_BeingLessThan_Zero()
+    [TestCase(1, -2, true)]
+    [TestCase(1, -2, false)]
+    [TestCase(7, -1, true)]
+    [TestCase(7, -1, false)]
+    public void NumberValidator_ThrowsOnCreation_WithScale_BeingLessThan_Zero(
+        int precision,
+        int scale,
+        bool onlyPositive)
     {
-        using (new AssertionScope())
-        {
-            new Action(() => new NumberValidator(1, -2, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(1, -2, false)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, -1, true)).Should().Throw<ArgumentException>();
-            new Action(() => new NumberValidator(7, -1, false)).Should().Throw<ArgumentException>();
-        }
+        new Action(() => new NumberValidator(precision, scale, onlyPositive))
+            .Should()
+            .Throw<ArgumentException>()
+            .WithMessage("precision must be a non-negative number less or equal than precision");
     }
 
     [Test]
-    public void IsValidNumber_ReturnsTrue_ForOnlyPositiveCorrectNumbers()
+    [TestCaseSource(nameof(OnlyPositiveValidNumberCases))]
+    [TestCaseSource(nameof(OnlyPositiveInvalidNumberCases))]
+    [TestCaseSource(nameof(GeneralValidNumberCases))]
+    [TestCaseSource(nameof(GeneralInvalidNumberCases))]
+    public void IsValidNumber_Returns_ExpectedResult(
+        int precision,
+        int scale,
+        bool onlyPositive,
+        string number,
+        bool expectedResult)
     {
-        var numberValidator = new NumberValidator(4, 2, true);
-        var validNumbers = new[]
-        {
-            "0.0", "0,0", "0", "1.23", "+0.00", "01.10", "+0,00", "1,0"
-        };
-        
-        var numberValidatorWithScaleOfZero = new NumberValidator(2, 0, true);
-        var validNumbersWithScaleOfZero = new[]
+        new NumberValidator(precision, scale, onlyPositive)
+            .IsValidNumber(number)
+            .Should()
+            .Be(expectedResult);
+    }
+
+    public static object[] OnlyPositiveValidNumberCases()
+    {
+        var onlyPositiveCasesWithScaleOfZero = new[]
         {
             "1", "+0", "01", "00"
-        };
-
-
-        using (new AssertionScope())
+        }.Select(number => new object[]
         {
-            validNumbers.Should().OnlyContain(x => numberValidator.IsValidNumber(x));
-            validNumbersWithScaleOfZero.Should().OnlyContain(x => numberValidatorWithScaleOfZero.IsValidNumber(x));
-        }
+            2, 0, true, number, true
+        });
+            
+        var onlyPositiveCases = new[]
+        {
+            "0.0", "0,0", "0", "1.23", "+0.00", "01.10", "+0,00", "1,0"
+        }.Select(number => new object[]
+        {
+            4, 2, true, number, true
+        });
+
+        return onlyPositiveCasesWithScaleOfZero
+            .Concat(onlyPositiveCases)
+            .ToArray<object>();
     }
 
-    [Test]
-    public void IsValidNumber_ReturnsFalse_ForIncorrectNumbers_And_ForNegativeNumbers()
+    public static object[] OnlyPositiveInvalidNumberCases()
     {
-        var numberValidator = new NumberValidator(3, 2, true);
-        var invalidNumbers = new[]
+        var numbers = new[]
         {
             "000.0", "-0.00", "+0,00", "+1.23", "-1,23", "a.sd",
             "0.000", "0.", ",00", "", null, "    ", " ", "-", "+",
             "   1.5", "0,2   ", "0  . 11"
         };
-        
-        var numberValidatorWithScaleOfZero = new NumberValidator(3, 0, true);
-        var invalidNumbersWithScaleOfZero = new List<string>
-        {
-            "-1", "+000", "0.0", "3.14", "-0.1"
-        };
-        invalidNumbersWithScaleOfZero.AddRange(invalidNumbers!);
 
-        
-        using (new AssertionScope()) {
-            invalidNumbers.Should().OnlyContain(x => !numberValidator.IsValidNumber(x));
-            invalidNumbersWithScaleOfZero.Should().OnlyContain(x => !numberValidatorWithScaleOfZero.IsValidNumber(x));
-        }
+        var onlyPositiveCasesWithScaleOfZero =
+            new[]
+                {
+                    "-1", "+000", "0.0", "3.14", "-0.1"
+                }
+                .Concat(numbers)
+                .Select(number => new object[]
+                {
+                    3, 0, true, number, false
+                });
+
+        var onlyPositiveCases =
+            numbers.Select(number => new object[]
+            {
+                3, 2, true, number, false
+            });
+
+        return onlyPositiveCasesWithScaleOfZero
+            .Concat(onlyPositiveCases)
+            .ToArray<object>();
     }
 
-    [Test]
-    public void IsValidNumber_ReturnsTrue_ForAnyCorrectNumber()
+    public static object[] GeneralValidNumberCases()
     {
-        var numberValidator = new NumberValidator(4, 2, false);
-        var validNumbers = new[]
-        {
-            "0.0", "0", "-0", "-1.23", "+0.00", "01.10", "-1,10", "+10.0"
-        };
-        
-        var numberValidatorWithScaleOfZero = new NumberValidator(2, 0, false);
-        var validNumbersWithScaleOfZero = new[]
+        var onlyPositiveCasesWithScaleOfZero = new[]
         {
             "1", "+0", "01", "00", "-0", "+2", "-2"
-        };
+        }.Select(number => new object[]
+        {
+            2, 0, false, number, true
+        });
+            
+        var onlyPositiveCases = new[]
+        {
+            "0.0", "0", "-0", "-1.23", "+0.00", "01.10", "-1,10", "+10.0"
+        }.Select(number => new object[]
+        {
+            4, 2, false, number, true
+        });
 
-
-        using (new AssertionScope()) {
-            validNumbers.Should().OnlyContain(x => numberValidator.IsValidNumber(x));
-            validNumbersWithScaleOfZero.Should().OnlyContain(x => numberValidatorWithScaleOfZero.IsValidNumber(x));
-        }
+        return onlyPositiveCasesWithScaleOfZero
+            .Concat(onlyPositiveCases)
+            .ToArray<object>();
     }
 
-    [Test]
-    public void IsValidNumber_ReturnsFalse_ForAnyIncorrectNumber()
+    public static object[] GeneralInvalidNumberCases()
     {
-        var numberValidator = new NumberValidator(3, 2, false);
-        var invalidNumbers = new[]
+        var numbers = new[]
         {
             "000.0", "-0.00", "+0,00", "+1.23", "-1,23", "a.sd",
             "0.000", "0.", ",00", "", null, "    ", " ", "-,0",
             "+0,", "   1.5", "0,2   ", "0  . 11"
         };
-        
-        var numberValidatorWithScaleOfZero = new NumberValidator(3, 0, false);
-        var invalidNumbersWithScaleOfZero = new List<string>
-        {
-            "-1.0", "+000", "0.0", "3.14", "-0.1", "1111"
-        };
-        invalidNumbersWithScaleOfZero.AddRange(invalidNumbers!);
-        
-        
-        using (new AssertionScope()) {
-            invalidNumbers.Should().OnlyContain(x => !numberValidator.IsValidNumber(x));
-            invalidNumbersWithScaleOfZero.Should().OnlyContain(x => !numberValidatorWithScaleOfZero.IsValidNumber(x));
-        }
+
+        var onlyPositiveCasesWithScaleOfZero =
+            new[]
+                {
+                    "-1.0", "+000", "0.0", "3.14", "-0.1", "1111"
+                }
+                .Concat(numbers)
+                .Select(number => new object[]
+                {
+                    3, 0, false, number, false
+                });
+
+        var onlyPositiveCases =
+            numbers.Select(number => new object[]
+            {
+                3, 2, false, number, false
+            });
+
+        return onlyPositiveCasesWithScaleOfZero
+            .Concat(onlyPositiveCases)
+            .ToArray<object>();
     }
 }
