c64823c978337685ccb20007cc3081d679a273b9
SomEnaMeforme <rushkova909@gmail.com>
2024-11-05 15:24:56 +0500

    Add NumberValidator constructor tests

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,6 +1,8 @@
 ﻿
+using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
+using System;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
@@ -10,10 +12,10 @@ public class NumberValidatorTests
     [Test]
     public void Test()
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        //Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
+        //Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        //Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
+        //Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
         ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
         ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
@@ -28,4 +30,55 @@ public class NumberValidatorTests
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
     }
+
+    [Test]
+    public void NumberValidator_WhenPrecisionIsNegative_Fails()
+    {
+        Action act = () => new NumberValidator(-1, 2, false);
+
+        act
+            .Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a positive number");
+    }
+
+    [Test]
+    public void NumberValidator_WhenPrecisionIsZero_Fails()
+    {
+        Action act = () => new NumberValidator(0, 2, false);
+
+        act
+            .Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a positive number");
+    }
+
+    [Test]
+    public void NumberValidator_WhenScaleIsZero_NotFails()
+    {
+        Action act = () => new NumberValidator(1, 0, true);
+
+        act
+            .Should().NotThrow();
+    }
+
+    [Test]
+    public void NumberValidator_WhenScaleIsNegative_Fails()
+    {
+        Action act = () => new NumberValidator(1, -1, false);
+
+        act
+            .Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a non-negative number less or equal than precision");
+    }
+
+    [Test]
+    public void NumberValidator_WhenPrecisionLessThanScale_Fails()
+    {
+        Action act = () => new NumberValidator(1, 2, false);
+
+        act
+            .Should().Throw<ArgumentException>()
+            .WithMessage("precision must be a non-negative number less or equal than precision");
+    }
+
+    
 }
