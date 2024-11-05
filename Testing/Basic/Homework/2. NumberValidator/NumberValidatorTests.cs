9a1a895ca9aaa7e305e67d48d3132d6e73d90d91
S4MPAI <flekshum@gmail.com>
2024-11-05 23:07:49 +0500

    add TestName in TestCases

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -6,11 +6,11 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [TestCase(-1, 5)]
-    [TestCase(5, -1)]
-    [TestCase(2, 5)]
-    [TestCase(2, 2)]
-    public void Should_Throw_Exception_With_Incorrect_Parameters(int precision, int scale)
+    [TestCase(-1, 5, TestName = "Should_Throw_ArgumentException_If_Precision_Is_Negative")]
+    [TestCase(5, -1, TestName = "Should_Throw_ArgumentException_If_Scale_Is_Negative")]
+    [TestCase(2, 5, TestName = "Should_Throw_ArgumentException_If_Scale_Greater_Precision")]
+    [TestCase(2, 2, TestName = "Should_Throw_ArgumentException_If_Scale_Equal_Precision")]
+    public void Should_Throw_Exception(int precision, int scale)
     {
         var act = () => new NumberValidator(precision, scale);
         
@@ -25,18 +25,18 @@ public class NumberValidatorTests
         act.Should().NotThrow<ArgumentException>();
     }
 
-    [TestCase(null)]
-    [TestCase(" ")]
-    [TestCase("a.sd")]
-    [TestCase("0.")]
-    [TestCase(",0")]
-    [TestCase("0*0")]
-    [TestCase("!0,0")]
-    [TestCase("1,234")]
-    [TestCase("11111")]
-    [TestCase("22,32")]
-    [TestCase("+14,23")]
-    [TestCase("-0")]
+    [TestCase(null, TestName = "IsValidNumber_Should_Return_False_If_Value_Is_Null")]
+    [TestCase(" ", TestName = "IsValidNumber_Should_Return_False_If_Value_Is_Empty")]
+    [TestCase("2.a1", TestName = "IsValidNumber_Should_Return_False_If_Value_Contains_Letter")]
+    [TestCase("0.", TestName = "IsValidNumber_Should_Return_False_If_Value_Contains_Separator_Without_FracPart")]
+    [TestCase(",0", TestName = "IsValidNumber_Should_Return_False_If_Value_Without_IntPart")]
+    [TestCase("0*0", TestName = "IsValidNumber_Should_Return_False_If_Value_Separator_Incorrect")]
+    [TestCase("!0,0", TestName = "IsValidNumber_Should_Return_False_If_Value_Sign_Incorrect")]
+    [TestCase("1,234", TestName = "IsValidNumber_Should_Return_False_If_Value_FracPart_More_Than_Scale")]
+    [TestCase("11111", TestName = "IsValidNumber_Should_Return_False_If_Value_IntPart_More_Than_Precision")]
+    [TestCase("225,32", TestName = "IsValidNumber_Should_Return_False_If_Value_With_FracPart_More_Than_Precision")]
+    [TestCase("+14,23", TestName = "IsValidNumber_Should_Return_False_If_Value_With_Sign_More_Than_Precision")]
+    [TestCase("-0", TestName = "IsValidNumber_Should_Return_False_If_OnlyPositive_True_And_Value_Contains_Minus")]
     public void IsValidNumber_Should_Return_False(string value)
     {
         var numberValidator = new NumberValidator(4, 2, true);
@@ -46,10 +46,10 @@ public class NumberValidatorTests
         actual.Should().BeFalse();
     }
     
-    [TestCase("-1,4", false)]
-    [TestCase("1")]
-    [TestCase("+1,444")]
-    [TestCase("1,444")]
+    [TestCase("-1,4", false, TestName = "IsValidNumber_Should_Return_True_If_OnlyPositive_False_And_Value_Contains_Minus")]
+    [TestCase("1", TestName = "IsValidNumber_Should_Return_True_If_Value_IntPart")]
+    [TestCase("+1,444", TestName = "IsValidNumber_Should_Return_True_If_Value_Contains_FracPart_And_Sign")]
+    [TestCase("1,444", TestName = "IsValidNumber_Should_Return_True_If_Value_Contains_FracPart")]
     public void IsValidNumber_Should_Return_True(string value, bool onlyPositive = true)
     {
         var numberValidator = new NumberValidator(10, 5, onlyPositive);
