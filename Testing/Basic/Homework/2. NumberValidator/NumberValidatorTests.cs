104c9c05f161ed8cb2a2efdf5f4393337ea75d2e
maakhhh <d.makhlonov@mail.ru>
2024-11-05 20:27:27 +0500

    Complete Homework for Testing

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,6 +1,5 @@
-﻿
-using NUnit.Framework;
-using NUnit.Framework.Legacy;
+﻿using NUnit.Framework;
+using FluentAssertions;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
@@ -8,24 +7,40 @@ namespace HomeExercise.Tasks.NumberValidator;
 public class NumberValidatorTests
 {
     [Test]
-    public void Test()
+    [TestCase(-1, 0, TestName = "NegativePrecision")]
+    [TestCase(0, 0, TestName = "ZeroPrecision")]
+    [TestCase(1, -1, TestName = "NegativeScale")]
+    [TestCase(1, 2, TestName = "ScaleGreraterThenPrecision")]
+    public void ValidateConstructor_ThrowsException_WithUncorrectData(int precision, int scale)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+        Action act = () => new NumberValidator(precision, scale);
+        act.Should().Throw<ArgumentException>();
+    }
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [Test]
+    [TestCase(1, 0, TestName = "CorrectData")]
+    public void ValidateConstructor_DontThrowsException_WithCorrectData(int precision, int scale)
+    {
+        Action act = () => new NumberValidator(precision, scale);
+        act.Should().NotThrow();
+    }
+
+    [Test]
+    [TestCase("0.0", true, true, TestName = "SimpleCorrectValueWithPoint")]
+    [TestCase("0,0", true, true, TestName = "SimpleCorrectValueWithComma")]
+    [TestCase("0", true, true, TestName = "SimpleDataWithoutValue")]
+    [TestCase("0.0", true, true, TestName = "SimpleCorrectValue")]
+    [TestCase("000.00", true, false, TestName = "TooMuchNumbers")]
+    [TestCase("0.000", true, false, TestName = "BigFracPart")]
+    [TestCase("-0.0", true, false, TestName = "NegativeValueForPositiveValidator")]
+    [TestCase("-0.0", false, true, TestName = "NegativeValueForNegativeValidator")]
+    [TestCase("+0.000", true, false, TestName = "PlusMustIncludeInPrecision")]
+    [TestCase("a.asd", true, false, TestName = "UncorrectValue")]
+    [TestCase("", true, false, TestName = "EmptyValue")]
+    [TestCase(null, true, false, TestName = "NullValue")]
+    public void Validator_IsValidate(string value, bool onlyPositive, bool expectedResult)
+    {
+        var validatorResult = new NumberValidator(4, 2, onlyPositive).IsValidNumber(value);
+        validatorResult.Should().Be(expectedResult, $"Проверка числа { value } для валидатора (4, 2, {onlyPositive})");
     }
 }
