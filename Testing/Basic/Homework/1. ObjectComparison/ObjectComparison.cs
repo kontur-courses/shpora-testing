c0d8c9e6f27191ec98e72936ab075c1f17c156e5
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-06 14:46:49 +0500

    Черных Илья

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
@@ -13,17 +14,12 @@ public class ObjectComparison
 
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
-
-        // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+        
+        actualTsar.Should().BeEquivalentTo(expectedTsar, options =>
+            options.Excluding(person => person.Id)
+                .Excluding(person => person.Parent.Id)
+                .AllowingInfiniteRecursion()
+        );
     }
 
     [Test]
@@ -35,6 +31,11 @@ public class ObjectComparison
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
         // Какие недостатки у такого подхода? 
+        // 1) При добавлении нового поля/свойства в класс нужно будет добавлять его проверку в метод AreEqual,
+        // если этого не сделать, то тест будет выполнять проверку не верно. В моём решении при добавлении поля/свойства
+        // тест менять не нужно (нужно менять только если мы хотим исключить проверку на это поле/свойство)
+        // 2) Если поле/свойство будет иметь тип класс, то нужно будет добавлять проверку на этот класс по полям/свойства
+        // 3) Если тест сломается, то будет неочевидно на каком именно поле/свойстве расхождение
         ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
     }
 
