8897becf8bf10b074a1283c2f37e18e21553c08f
Zakhar <zax_2000@mail.ru>
2024-11-06 19:50:27 +0500

    Структура тестов упрощена за счёт использования аргумента "ExpectedResult" в атрибуте [TestCase].

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -29,75 +29,75 @@ public class NumberValidatorTests
         Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale));
     }
 
-    [TestCase(17, 2, true, null)]
-    [TestCase(17, 2, true, "")]
-    [TestCase(17, 2, true, " ")]
-    public void IsValidNumber_ReturnFalse_WhenGivenNullOrEmptyString(int precision,
+    [TestCase(17, 2, true, null, ExpectedResult = false)]
+    [TestCase(17, 2, true, "", ExpectedResult = false)]
+    [TestCase(17, 2, true, " ", ExpectedResult = false)]
+    public bool IsValidNumber_ReturnFalse_WhenGivenNullOrEmptyString(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == false);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(17, 2, true, "0.0")]
-    [TestCase(17, 2, true, "0")]
-    [TestCase(4, 2, true, "+1.23")]
-    public void IsValidNumber_ReturnTrue_WhenGivenCorrectValues(int precision,
+    [TestCase(17, 2, true, "0.0", ExpectedResult = true)]
+    [TestCase(17, 2, true, "0", ExpectedResult = true)]
+    [TestCase(4, 2, true, "+1.23", ExpectedResult = true)]
+    public bool IsValidNumber_ReturnTrue_WhenGivenCorrectValues(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == true);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(17, 2, true, "0,0")]
-    public void IsValidNumber_ReturnTrue_WhenGivenCorrectValuesWithComma(int precision,
+    [TestCase(17, 2, true, "0,0", ExpectedResult = true)]
+    public bool IsValidNumber_ReturnTrue_WhenGivenCorrectValuesWithComma(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == true);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(17, 2, true, "a.sd")]
-    [TestCase(17, 2, true, "1..2")]
-    [TestCase(17, 2, true, "1.2.3")]
-    [TestCase(17, 2, true, "1.a")]
-    [TestCase(17, 2, true, "a.1")]
-    [TestCase(17, 2, true, "0.,0")]
-    public void IsValidNumber_ReturnFalse_WhenGivenNotANumberValues(int precision,
+    [TestCase(17, 2, true, "a.sd", ExpectedResult = false)]
+    [TestCase(17, 2, true, "1..2", ExpectedResult = false)]
+    [TestCase(17, 2, true, "1.2.3", ExpectedResult = false)]
+    [TestCase(17, 2, true, "1.a", ExpectedResult = false)]
+    [TestCase(17, 2, true, "a.1", ExpectedResult = false)]
+    [TestCase(17, 2, true, "0.,0", ExpectedResult = false)]
+    public bool IsValidNumber_ReturnFalse_WhenGivenNotANumberValues(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == false);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(3, 2, true, "00.00")]
-    [TestCase(3, 0, true, "0.000")]
-    [TestCase(3, 2, false, "-1.23")]
-    public void IsValidNumber_ReturnFalse_WhenPrecisionIsExceeded(int precision,
+    [TestCase(3, 2, true, "00.00", ExpectedResult = false)]
+    [TestCase(3, 0, true, "0.000", ExpectedResult = false)]
+    [TestCase(3, 2, false, "-1.23", ExpectedResult = false)]
+    public bool IsValidNumber_ReturnFalse_WhenPrecisionIsExceeded(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == false);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(17, 2, true, "0.000")]
-    public void IsValidNumber_ReturnFalse_WhenScaleIsExceeded(int precision,
+    [TestCase(17, 2, true, "0.000", ExpectedResult = false)]
+    public bool IsValidNumber_ReturnFalse_WhenScaleIsExceeded(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == false);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(3, 2, true, "-0.00")]
-    public void IsValidNumber_ReturnFalse_WhenInvalidNumberSign(int precision,
+    [TestCase(3, 2, true, "-0.00", ExpectedResult = false)]
+    public bool IsValidNumber_ReturnFalse_WhenInvalidNumberSign(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == false);
+        return validator.IsValidNumber(value);
     }
 
-    [TestCase(3, 2, true, "+1.23")]
-    [TestCase(3, 2, true, "1.23")]
+    [TestCase(3, 2, true, "+1.23", ExpectedResult = true)]
+    [TestCase(3, 2, true, "1.23", ExpectedResult = true)]
     //  Я думаю, что в данном задании подразумивается,
     //  что NumberValidator корректен.
     //  Тогда цитирую:
@@ -109,10 +109,10 @@ public class NumberValidatorTests
     //  возвращать true, так как +1.23 это положительное число,
     //  а знак включаться в длину должен только у отрицательного числа.
     //  Я не так понял?
-    public void IsValidNumber_ReturnTrue_WhenGivenCorrectPositiveValue(int precision,
+    public bool IsValidNumber_ReturnTrue_WhenGivenCorrectPositiveValue(int precision,
         int scale, bool onlyPositive, string value)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        Assert.That(validator.IsValidNumber(value) == true);
+        return validator.IsValidNumber(value);
     }
 }
