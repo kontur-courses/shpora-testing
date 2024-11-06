7933bda10b40464f1f2d781b82a972922fa50029
kexogg <kexogg@gmail.com>
2024-11-06 22:55:10 +0500

    Refactor tests to address comments in PR

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -6,48 +6,59 @@ namespace HomeExercise.Tasks.NumberValidator;
 [TestFixture]
 public class NumberValidatorTests
 {
-    [TestCase(17, 2, true, "0.0", ExpectedResult = true, TestName = "Valid zero")]
-    [TestCase(17, 2, true, "0", ExpectedResult = true, TestName = "Valid zero without fraction")]
-    [TestCase(3, 2, true, "00.00", ExpectedResult = false, TestName = "Invalid zero with leading zero")]
-    [TestCase(3, 2, true, "-0.00", ExpectedResult = false, TestName = "Invalid zero with negative sign")]
-    [TestCase(3, 2, true, "+0.00", ExpectedResult = false, TestName = "Invalid zero with positive sign")]
-    [TestCase(17, 2, true, "0.000", ExpectedResult = false, TestName = "Invalid zero with tailing zeroes")]
-    [TestCase(1, 0, true, "0", ExpectedResult = true, TestName = "Valid zero with precision = 1 and scale = 0")]
-    [TestCase(2, 0, true, "+0", ExpectedResult = true, TestName = "Valid zero with plus sign, precision = 2, scale = 0")]
-    [TestCase(1, 0, true, "+0", ExpectedResult = false, TestName = "Invalid zero with plus sign, precision = 2, scale = 0")]
-    [TestCase(1, 0, true, "-0", ExpectedResult = false, TestName = "Invalid zero with minus sign, precision = 2, scale = 0")]
-
-    // Positive number cases
-    [TestCase(4, 2, true, "+1.23", ExpectedResult = true, TestName = "Valid positive number with positive sign")]
-    [TestCase(3, 2, true, "1.23", ExpectedResult = true, TestName = "Valid positive number without sign")]
-    [TestCase(3, 2, false, "1.23", ExpectedResult = true, TestName = "Valid positive number without sign")]
-    [TestCase(3, 2, true, "+1.23", ExpectedResult = false, TestName = "Invalid number with positive sign")]
-
-    // Negative number cases
-    [TestCase(3, 2, false, "-1.23", ExpectedResult = false, TestName = "Invalid negative number")]
-    [TestCase(4, 3, false, "-1.23", ExpectedResult = true, TestName = "Valid negative number")]
-    [TestCase(4, 3, true, "-1.23", ExpectedResult = false, TestName = "Invalid negative number with onlyPositive = true")]
-    [TestCase(2, 1, false, "-1", ExpectedResult = true, TestName = "Valid negative number with precision = 2 and scale = 1")]
-
-    // Other cases
-    [TestCase(1, 0, true, "", ExpectedResult = false, TestName = "Empty string")]
-    [TestCase(3, 2, true, "a.sd", ExpectedResult = false, TestName = "Letters instead of number")]
-    [TestCase(3, 2, true, null, ExpectedResult = false, TestName = "Null string")]
-    [TestCase(3, 2, true, "0,0", ExpectedResult = true, TestName = "Valid with comma as point")]
-
-    public bool NumberValidator_IsValidNumber(int precision, int scale, bool onlyPositive, string value)
+    [TestCase(17, 2, true, "0.0", TestName = "Valid zero")]
+    [TestCase(17, 2, true, "00.0", TestName = "Zero with leading zero")]
+    [TestCase(1, 0, true, "0", TestName = "Zero with small precision")]
+    public void IsValidNumber_ShouldBeTrue_WhenCorrectZero(int precision, int scale, bool onlyPositive, string value)
     {
-        return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+        new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeTrue();
     }
 
-    [TestCase(-1, 2, true, true)]
-    [TestCase(1, -1, true, true)]
-    [TestCase(1, 2, true, true)]
-
-    public void NumberValidator_ThrowsException(int precision, int scale, bool onlyPositive, bool shouldThrow)
+    [TestCase(3, 2, "1.23", TestName = "Valid number")]
+    [TestCase(3, 2, "1,23", TestName = "Number with comma as point")]
+    public void IsValidNumber_ShouldBeTrue_WhenCorrectNumber(int precision, int scale, string value)
     {
-        var validator = () => new NumberValidator(precision, scale, onlyPositive);
-        if (shouldThrow) validator.Should().ThrowExactly<ArgumentException>();
-        else validator.Should().NotThrow();
+        new NumberValidator(precision, scale, true).IsValidNumber(value).Should().BeTrue();
+    }
+
+    [TestCase(4, 2, true, "+1.23", TestName = "Positive number")]
+    [TestCase(4, 3, false, "-1.23", TestName = "Negative number")]
+    public void IsValidNumber_ShouldBeTrue_WhenCorrectSigned(int precision, int scale, bool onlyPositive, string value)
+    {
+        new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeTrue();
+    }
+
+    [TestCase(4, 3, "-1.23", TestName = "Negative number when only positive flag is set")]
+    public void IsValidNumber_ShouldBeFalse_WhenIncorrectSigned(int precision, int scale, string value)
+    {
+        new NumberValidator(precision, scale, true).IsValidNumber(value).Should().BeFalse();
+    }
+
+    [TestCase(1, 0, "", TestName = "Empty string")]
+    [TestCase(1, 0, " ", TestName = "Whitespace")]
+    [TestCase(3, 2, "-", TestName = "Only negative sign")]
+    [TestCase(3, 2,  "a.sd", TestName = "Letters instead of number")]
+    [TestCase(3, 2,  null, TestName = "Null string")]
+    public void IsValidNumber_ShouldBeFalse_WhenNotANumber(int precision, int scale, string value)
+    {
+        new NumberValidator(precision, scale).IsValidNumber(value).Should().BeFalse();
+    }
+
+    [TestCase(2, 1, true, "00.0", TestName = "Zero with leading zero")]
+    [TestCase(17, 2, true, "0.000", TestName = "Zero with tailing zeroes")]
+    [TestCase(1, 0, true, "+0", TestName = "Zero with plus sign")]
+    public void IsValidNumber_ShouldBeFalse_WhenOverflow(int precision, int scale, bool onlyPositive,
+        string value)
+    {
+        new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value).Should().BeFalse();
+    }
+
+    [TestCase(-1, 2, TestName = "Negative precision")]
+    [TestCase(1, -1, TestName = "Negative scale")]
+    [TestCase(1, 2, TestName = "Scale greater than precision")]
+    public void Constructor_ShouldThrowException_WhenInvalidParams(int precision, int scale)
+    {
+        var validator = () => new NumberValidator(precision, scale);
+        validator.Should().ThrowExactly<ArgumentException>();
     }
 }
