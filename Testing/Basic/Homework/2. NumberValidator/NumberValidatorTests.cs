63aeed48b3408166243a855e090c58524150b7ca
stepan <stepan.zakolyukin@yandex.ru>
2024-11-06 15:54:07 +0500

    Разбил тесты относящиеся к тестированию метода IsValidNumber на категории ошибок, которые они проверяют, удалил повторяющиеся тесты, дополнил решение класс своими тестами

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -26,21 +26,46 @@ public class NumberValidatorTests
         Assert.Throws<ArgumentException>(() => new NumberValidator(1, 2, true));
         Assert.Throws<ArgumentException>(() => new NumberValidator(1, 1, true));
     }
-    
+
     [Test]
-    public void Test()
+    public void IsValidNumber_InvalidCharacters_False()
     {
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
         ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    }
+
+    [TestCase("00.00", 3, 2)]
+    [TestCase("+1.23", 3, 2)]
+    [TestCase("0.000", 17, 2)]
+    public void IsValidNumber_NotTheAppropriateLength_False(string value, int precision, int scale = 0, bool onlyPositive = true)
+    {
+        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value));
+    }
+
+    [TestCase("+1.23", 4, 2)]
+    [TestCase("0", 17, 2)]
+    [TestCase("0.0", 17, 2)]
+    public void IsValidNumber_CorrectValue_True(string value, int precision, int scale = 0, bool onlyPositive = true)
+    {
+        ClassicAssert.IsTrue(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value));
+    }
+
+    [TestCase("-0.00", 3, 2)]
+    [TestCase("+0.00", 3, 2, false)]
+    public void IsValidNumber_TheWrongSign_False(string value, int precision, int scale = 0, bool onlyPositive = true)
+    {
+        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value));
+    }
+
+    [TestCase("", 1)]
+    [TestCase(null, 1)]
+    public void IsValidNumber_NullOrEmpty_False(string value, int precision, int scale = 0, bool onlyPositive = true)
+    {
+        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value));
+    }
+
+    [Test]
+    public void IsValidNumber_InappropriateAccuracy_False()
+    {
+        ClassicAssert.IsFalse(new NumberValidator(4, 2, true).IsValidNumber("0.000"));
     }
 }
