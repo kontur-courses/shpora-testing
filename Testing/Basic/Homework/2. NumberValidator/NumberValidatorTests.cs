cee3d86f1dfb0bb5ba76e85f19b3c05c8522c4d5
stepan <stepan.zakolyukin@yandex.ru>
2024-11-07 16:19:05 +0500

    Отрефакторил тесты

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,81 +1,99 @@
 ﻿
+using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void NumberValidator_IncorrectPrecision_Exception()
+    [TestCase(false)]
+    [TestCase(true)]
+    public void NumberValidator_IncorrectPrecision_Exception(bool onlyPositive)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true),
-            "precision должен быть положительным числом");
+        var createAConstructor = () => new NumberValidator(-1, 2, onlyPositive);
+        createAConstructor
+            .Should()
+            .Throw<ArgumentException>("precision должен быть положительным числом");
     }
 
-    [Test]
-    public void NumberValidator_CorrectInitialization_NoExceptions()
+    [TestCase(true)]
+    [TestCase(false)]
+    public void NumberValidator_CorrectInitialization_NoExceptions(bool onlyPositive)
     {
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true),
-            "При создании экземпляра класса не должно было возникнуть ошибок");
+        var createAConstructor = () => new NumberValidator(3, 2, onlyPositive);
+        createAConstructor
+            .Should()
+            .NotThrow();
     }
 
     [TestCase(1, -1)]
     [TestCase(1, 2)]
     [TestCase(1, 1)]
-    public void NumberValidator_IncorrectScale_Exception(int precision, int scale = 0, bool onlyPositive = true)
+    public void NumberValidator_IncorrectScale_Exception(int precision, int scale)
     {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(precision, scale, onlyPositive),
-            "scale должно быть меньше precision, но больше или равно 0");
+        var createAConstructor = () => new NumberValidator(precision, scale, true);
+        createAConstructor
+            .Should()
+            .Throw<ArgumentException>("scale должно быть меньше precision, но больше или равно 0");
     }
 
     [Test]
     public void IsValidNumber_InvalidCharacters_False()
     {
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"),
-            "В записи числа не должны содержаться буквы");
+        new NumberValidator(3, 2, true)
+            .IsValidNumber("a.sd")
+            .Should()
+            .BeFalse("В записи числа не должны содержаться буквы");
     }
 
     [TestCase("00.00", 3, 2)]
     [TestCase("+1.23", 3, 2)]
-    public void IsValidNumber_NotTheAppropriateLength_False(string value,
-        int precision,
-        int scale = 0,
-        bool onlyPositive = true)
+    public void Should_ReturnFalse_WhenUnappropriateLength(string value, int precision, int scale)
     {
-        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value),
-            "Длина числа не соответствует шаблону");
+        new NumberValidator(precision, scale, true)
+            .IsValidNumber(value)
+            .Should()
+            .BeFalse("Длина числа не соответствует шаблону");
     }
 
     [TestCase("+1.23", 4, 2)]
     [TestCase("0", 17, 2)]
     [TestCase("0.0", 17, 2)]
-    public void IsValidNumber_CorrectValue_True(string value, int precision, int scale = 0, bool onlyPositive = true)
+    public void IsValidNumber_CorrectValue_True(string value, int precision, int scale)
     {
-        ClassicAssert.IsTrue(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value));
+        new NumberValidator(precision, scale, true)
+            .IsValidNumber(value)
+            .Should()
+            .BeTrue();
     }
 
     [TestCase("-0.00", 3, 2)]
     [TestCase("+0.00", 3, 2, false)]
     public void IsValidNumber_TheWrongSign_False(string value, int precision, int scale = 0, bool onlyPositive = true)
     {
-        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value),
-            "Знаки value и NumberValidator отличаются");
+        new NumberValidator(precision, scale, onlyPositive)
+            .IsValidNumber(value)
+            .Should()
+            .BeFalse("Знаки value и NumberValidator отличаются");
     }
 
-    [TestCase("", 1)]
-    [TestCase(null, 1)]
-    public void IsValidNumber_NullOrEmpty_False(string value, int precision, int scale = 0, bool onlyPositive = true)
+    [TestCase("")]
+    [TestCase(null)]
+    public void IsValidNumber_NullOrEmpty_False(string value, int precision = 1, int scale = 0, bool onlyPositive = true)
     {
-        ClassicAssert.IsFalse(new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value),
-            "Проверяемое значение не должно равняться null или пустой строке");
+        new NumberValidator(precision, scale, onlyPositive)
+            .IsValidNumber(value)
+            .Should()
+            .BeFalse("Проверяемое значение не должно равняться null или пустой строке");
     }
     
     [Test]
     public void IsValidNumber_InappropriateAccuracy_False()
     {
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"),
-            "значение не может иметь точность выше указанной в NumberValidator");
+        new NumberValidator(17, 2, true)
+            .IsValidNumber("0.000")
+            .Should()
+            .BeFalse("значение не может иметь точность выше указанной в NumberValidator");
     }
 }
