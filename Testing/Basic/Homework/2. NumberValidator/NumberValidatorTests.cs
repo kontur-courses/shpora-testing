5c9c2b639de3ceb08df770e9dd2aa5c698ed90bd
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-07 14:32:15 +0500

    Правки тестов в NumberValidatorTests Разделил тесты на несколько частей, добавил краевых случаев

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -34,23 +34,89 @@ public class NumberValidatorTests
     }
 
     [Test]
-    public void Constructor_ShouldNotThrowExceprion_WhenInputValuesAreValid()
+    public void Constructor_ShouldNotThrowException_WhenInputValuesAreValid()
     {
         Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
     }
 
-    [TestCase(4, 3, true, "", false)]
-    [TestCase(4, 3, true, null, false)]
-    [TestCase(3, 2, true, "a.sd", false)]
-    [TestCase(3, 2, true, "00.00", false)]
-    [TestCase(17, 2, true, "0.000", false)]
-    [TestCase(7, 3, true, "-1.23", false)]
-    [TestCase(17, 2, true, "0.0", true)]
-    [TestCase(17, 2, true, "0", true)]
-    [TestCase(4, 2, true, "+1.23", true)]
-    public void IsValidNumberTest(int precision, int scale, bool onlyPositive, string value, bool expectedResult)
+    [TestCase(4, 3, true, "")]
+    [TestCase(4, 3, true, null)]
+    [TestCase(4, 3, true, "a")]
+    [TestCase(4, 3, true, "a.sd")]
+    [TestCase(4, 3, true, "a.0")]
+    [TestCase(10, 7, true, "1.23a4")]
+    [TestCase(10, 7, true, "123.")]
+    public void IsValidNumber_ReturnFalse_WhenInputValueIsInvalidFormat(int precision, int scale, bool onlyPositive, string value)
     {
         var numberValidator = new NumberValidator(precision, scale, onlyPositive);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().Be(false);
+    }
+    
+    [TestCase(5, 2, "-123")]
+    [TestCase(5, 4, "-1.23")]
+    public void IsValidNumber_ReturnFalse_WhenValueIsNegativeAndOnlyPositiveIsTrue(int precision, int scale,
+        string value)
+    {
+        var numberValidator = new NumberValidator(precision, scale, true);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().Be(false);
+    }
+    
+    [TestCase(2, 0, "12", true)]
+    [TestCase(2, 1, "123", false)]
+    public void IsValidNumber_ReturnExpectedResult_WhenInputValueIsPositiveInteger(int precision, int scale,
+        string value, bool expectedResult)
+    {
+        var numberValidator = new NumberValidator(precision, scale, true);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().Be(expectedResult);
+    }
+    
+    [TestCase(3, 0, "-12", true)]
+    [TestCase(3, 1, "-123", false)]
+    public void IsValidNumber_ReturnExpectedResult_WhenInputValueIsNegativeInteger(int precision, int scale, string value, 
+        bool expectedResult)
+    {
+        var numberValidator = new NumberValidator(precision, scale);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().Be(expectedResult);
+    }
+    
+    [TestCase(8, 4, "12.34", true)]
+    [TestCase(5, 4, "11.234", true)]
+    [TestCase(7, 3, "1.234", true)]
+    [TestCase(8, 4, "+123,4567", true)]
+    [TestCase(4, 2, "+12.34", false)]
+    [TestCase(6, 2, "1.234", false)]
+    public void IsValidNumber_ReturnExpectedResult_WhenInputValueIsPositiveDouble(int precision, int scale, string value, 
+        bool expectedResult)
+    {
+        var numberValidator = new NumberValidator(precision, scale, true);
+
+        var isValidNumber = numberValidator.IsValidNumber(value);
+
+        isValidNumber.Should().Be(expectedResult);
+    }
+    
+    [TestCase(5, 4, "-12.3", true)]
+    [TestCase(8, 4, "-1234.567", true)]
+    [TestCase(12, 4, "-123.4567", true)]
+    [TestCase(6, 3, "-12,345", true)]
+    [TestCase(4, 1, "-123,4", false)]
+    [TestCase(10, 2, "-123.456", false)]
+    public void IsValidNumber_ReturnExpectedResult_WhenInputValueIsNegativeDouble(int precision, int scale,
+        string value, bool expectedResult)
+    {
+        var numberValidator = new NumberValidator(precision, scale);
 
         var isValidNumber = numberValidator.IsValidNumber(value);
 
