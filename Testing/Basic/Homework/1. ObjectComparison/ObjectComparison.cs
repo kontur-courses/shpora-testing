4e7e771e1f4bb42e38eaeda08587ff9a4d80ffdb
Савелий <vildanov.s@bk.ru>
2024-11-08 16:27:58 +0500

    Refactor ObjectComparison.cs

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -3,24 +3,24 @@ using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
+
 public class ObjectComparison
 {
     [Test]
     [Description("Проверка текущего царя")]
     [Category("ToRefactor")]
-    public void CheckCurrentTsar()
+    public void TsarRegistry_ShouldBeEqual_WithCurrentTsar()
     {
         var actualTsar = TsarRegistry.GetCurrentTsar();
 
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
-        
-        actualTsar.Should().BeEquivalentTo(expectedTsar, options => options.
-            Excluding(x => x.Id).
-            Excluding(x=>x.Parent));
-        
-        expectedTsar.Parent.Should().BeEquivalentTo(actualTsar.Parent, options => options.
-            Excluding(x =>x.Id));
+
+        actualTsar.Should().BeEquivalentTo(expectedTsar,
+            options => options
+                .Excluding(x => x.Path.EndsWith("Id"))
+                .AllowingInfiniteRecursion()
+                .IncludingProperties());
     }
 
     [Test]
@@ -38,10 +38,7 @@ public class ObjectComparison
          *  1. Такая реализация теста не даёт никакой конкретики при обвале теста,
          *      он скажет только Success или Failed, но не причину, а точнее мы не узнаем
          *      какое конкретно поле класса Person показало несовпадение, в отличие от моего решения.
-         *  2. Масштабируемость. Метод AreEqual работает только для текущей реализации Person,
-         *      и при его изменении (добавление или удаление полей), нужно будет добавлять/убирать строки
-         *      и в AreEqual. Также можно и вовсе обойтись без него, что заметно повысит читаемость кода.
-         * 3. Ликвидность. Если нам потребуется сравнить каких-нибудь двух Person без определённого поля,
+         * 2. Ликвидность. Если нам потребуется сравнить каких-нибудь двух Person без определённого поля,
          *      тогда в текущем классе потребуется несколько реализаций AreEqual, что очень плохо.
          */
     }
