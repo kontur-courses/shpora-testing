ef5b983ec47708f2f24d34ddc730b6e277200dfd
Савелий <vildanov.s@bk.ru>
2024-11-08 16:27:58 +0500

    Refactor ObjectComparison.cs

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -3,24 +3,24 @@ using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
+
 public class ObjectComparison
 {
     [Test]
     [Description("Проверка текущего царя")]
     [Category("ToRefactor")]
-    public void CheckCurrentTsar()
+    public void TsarRegistry_ShouldBeEqual_WithCurrentTsar()
     {
         var actualTsar = TsarRegistry.GetCurrentTsar();
 
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
-        
-        actualTsar.Should().BeEquivalentTo(expectedTsar, options => options.
-            Excluding(x => x.Id).
-            Excluding(x=>x.Parent));
-        
-        expectedTsar.Parent.Should().BeEquivalentTo(actualTsar.Parent, options => options.
-            Excluding(x =>x.Id));
+
+        actualTsar.Should().BeEquivalentTo(expectedTsar,
+            options => options
+                .Excluding(x => x.Path.EndsWith("Id"))
+                .AllowingInfiniteRecursion()
+                .IncludingProperties());
     }
 
     [Test]
