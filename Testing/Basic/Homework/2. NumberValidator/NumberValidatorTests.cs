467e3e3af9dd6e9620b370f36883385e0f42760e
Савелий <vildanov.s@bk.ru>
2024-11-11 17:49:55 +0500

    Refactor NumberValidatorTests.cs

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -3,16 +3,14 @@ using NUnit.Framework;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
-[TestFixture]
 public class NumberValidatorTests
 {
     [TestCase(-1, 2, true, TestName = "Negative precision")]
     [TestCase(2, 5, true, TestName = "Precision less then scale")]
     [TestCase(2, -3, true, TestName = "Negative scale")]
     [TestCase(-5, -3, true, TestName = "Negative scale and Precision less then scale ")]
-    [TestCase(null, null, null, TestName = "Null input data")]
-    public void NumberValidator_ShouldThrowArgumentException_WithIncorrectData(int precision, int scale,
-        bool onlyPositive)
+    public void NumberValidator_ShouldThrowArgumentException_WithIncorrectData(
+        int precision, int scale, bool onlyPositive)
     {
         Action act = () => new NumberValidator(precision, scale, onlyPositive);
         act.Should().Throw<ArgumentException>();
@@ -29,8 +27,8 @@ public class NumberValidatorTests
     [TestCase(3, 0, true, "0", ExpectedResult = true)]
     [TestCase(4, 2, false, "-1.24", ExpectedResult = true)]
     [TestCase(4, 2, false, "-1.24", ExpectedResult = true)]
-    public bool NumberValidator_ShouldReturnTrue_WithCorrectData(int precision, int scale, bool onlyPositive,
-        String number)
+    public bool NumberValidator_ShouldReturnTrue_WithCorrectData(
+        int precision, int scale, bool onlyPositive, string number)
     {
         NumberValidator numberValidator = new NumberValidator(precision, scale, onlyPositive);
         return numberValidator.IsValidNumber(number);
@@ -46,8 +44,8 @@ public class NumberValidatorTests
     [TestCase(5, 2, true, "\r", ExpectedResult = false)]
     [TestCase(3, 0, true, "1 23", ExpectedResult = false)]
     [TestCase(3, 0, true, " 2.3", ExpectedResult = false)]
-    public bool NumberValidator_ShouldReturnFalse_WithInCorrectData(int precision, int scale, bool onlyPositive,
-        String number)
+    public bool NumberValidator_ShouldReturnFalse_WithInCorrectData(
+        int precision, int scale, bool onlyPositive, string number)
     {
         NumberValidator numberValidator = new NumberValidator(precision, scale, onlyPositive);
         return numberValidator.IsValidNumber(number);
